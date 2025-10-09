6a0804385ecaddd87ab6809da933d5d425a293d1
Denver Timlick <denvertimlick@gmail.com>
2025-10-09 11:44:40 -0600

    finished fucntionalities and began formatting

--- src/app/week-5/new-item.js
+++ src/app/week-5/new-item.js
@@ -1,9 +1,41 @@
 'use client';
 import { useState } from 'react';
 
+//New item object for name, quantity, category
 export default function NewItem() {
+  const [name, setName] = useState('');
   const [quantity, setQuantity] = useState(1);
+  const [category, setCategory] = useState('Produce');
 
+  //handle sumbit fucntion
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    //item object
+    const item = {
+      name: name.trim(),
+      quantity: quantity,
+      category: category,
+    };
+
+    //logging the item object
+    console.log('item sumbitted!', item);
+    //alert with current state
+    alert(
+      'Item sumbitted:\n' +
+        `Item: ${item.name}\n` +
+        `Quantity: ${item.quantity}\n` +
+        `Category: ${item.category}`
+    );
+
+    //resetting to inital values
+    setName('');
+    setQuantity(1);
+    setCategory('Produce');
+  }
+
+  //Quanitity functions
+  //increment by 1
   function increment() {
     if (quantity < 20) {
       setQuantity(quantity + 1);
@@ -17,34 +49,68 @@ export default function NewItem() {
     }
   }
 
-  //function to reset counter to orignal value
-  //   function resetCounter() {
-  //   setQuantity(quantity = useState);
-  // }
-
+  //return values
   return (
-    <div className="m-2 w-65 rounded-lg border-2 border-rose-300 bg-linear-to-r from-rose-900 to-rose-950 text-center">
-      <h2 className="font-mono">Add or Remove Item</h2>
-      <h3 className="m-2 font-mono"> Quantity: {quantity}</h3>
-      <text className="m-2 font-mono">Available from 1 - 20</text>
+    <div className="m-4 flex-col rounded-lg border-2 border-rose-300 bg-linear-to-r from-rose-900 to-rose-950 text-center">
+      <form onSubmit={handleSubmit}>
+        {/* Item Name Field */}
+        <section className="font-mono">
+          Item:
+          <input
+            className="rounded-m m-2 border-2 border-black bg-white pl-1 text-black"
+            type="text"
+            value={name}
+            required
+            onChange={(event) => setName(event.target.value)}
+            placeholder=" e.g. Milk, 4 L 🥛"
+          />
+        </section>
 
-      <button
-        className="m-2 rounded-lg border-1 bg-slate-900 px-4 py-2 hover:border-1 hover:border-black hover:bg-white hover:text-black"
-        onClick={() => {
-          increment();
-        }}
-      >
-        +1
-      </button>
+        {/* Quantity field */}
+        <section>
+          <h3 className="m-2 font-mono"> Quantity: {quantity}</h3>
+          {/* Increment */}
+          <button
+            className="m-2 rounded-lg border-1 bg-slate-900 px-4 py-2 hover:border-1 hover:border-black hover:bg-white hover:text-black"
+            onClick={increment}
+          >
+            +
+          </button>
+          {/* Decrement */}
+          <button
+            className="m-2 rounded-lg border-1 bg-slate-900 px-4 py-2 hover:border-1 hover:border-black hover:bg-white hover:text-black"
+            onClick={decrement}
+          >
+            -
+          </button>
+        </section>
+        <text className="m-2 font-mono">Available from 1 - 20</text>
 
-      <button
-        className="m-2 rounded-lg border-1 bg-slate-900 px-4 py-2 hover:border-1 hover:border-black hover:bg-white hover:text-black"
-        onClick={() => {
-          decrement();
-        }}
-      >
-        -1
-      </button>
+        {/* Category field */}
+        <section className="font-mono">
+          Category:
+          <select
+            className="rounded-m m-2 border-2 border-black bg-white pl-1 text-black"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="produce">Produce</option>
+            <option value="dairy">Dairy</option>
+            <option value="bakery">Bakery</option>
+            <option value="meat">Meat</option>
+            <option value="frozen foods">Frozen Foods</option>
+            <option value="canned goods">Canned Goods</option>
+            <option value="dry goods">Dry Goods</option>
+            <option value="beverages">Beverages</option>
+            <option value="snacks">Snacks</option>
+            <option value="household">Household</option>
+            <option value="other">Other</option>
+          </select>
+        </section>
+
+        {/* Sumbit Button */}
+        <button type="sumbit"> Submit </button>
+      </form>
     </div>
   );
 }
